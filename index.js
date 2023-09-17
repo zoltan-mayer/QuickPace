92e484a0cd720dff21a4a4973733219f5ba6eef6
Zoltan Mayer <zoltan.mayer@outlook.com>
2023-09-17 19:09:03 +0200

    Switched to jQuery for sliders.

--- index.js
+++ index.js
@@ -34,6 +34,28 @@ speed_table.rows[0].cells[3].style.width = (pace_table_row_width - speed_table_r
 // End: table formatting
 const one_mile = 1609.344;
 const dist_array = [1, 1000, one_mile, 5000, 10000, (10 * one_mile), 21097.5, (2 * 21097.5), 1, 1];
+const custom_unit_array = [1, 1000, 0.3048, one_mile];
+
+$(function () {
+    $("#coarseSlider").slider({
+        orientation: "vertical",
+        range: "min",
+        min: 4.5,
+        max: 24.5,
+        step: 0.001,
+        value: 12,
+        slide: on_coarse_slider_input
+    });
+    $("#fineSlider").slider({
+        orientation: "vertical",
+        range: "min",
+        min: -0.5,
+        max: 0.5,
+        step: 0.001,
+        value: 0,
+        slide: on_fine_slider_input
+    });
+});
 
 function update_pace_table() {
     let speed_mps = speed_kph_var / 3.6;
@@ -78,38 +100,35 @@ const cd_2_unit = document.getElementById("custom_unit_2");
 const kph_cell = document.getElementById("kphCell");
 const mph_cell = document.getElementById("mphCell");
 
-let speed_kph_var = parseFloat(Number(coarse_slider.value) + Number(fine_slider.value)).toFixed(5);
+let speed_kph_var = 12.0;
 kph_cell.innerHTML = parseFloat(speed_kph_var).toFixed(3);
 mph_cell.innerHTML = parseFloat(speed_kph_var * 0.6213712).toFixed(3);
 
-let custom_distance_1 = parseFloat(cd_1_val.value) * parseFloat(cd_1_unit.value);
-let custom_distance_2 = parseFloat(cd_2_val.value) * parseFloat(cd_2_unit.value);
+let custom_distance_1 = parseFloat(cd_1_val.value) * parseFloat(custom_unit_array[cd_1_unit.value]);
+let custom_distance_2 = parseFloat(cd_2_val.value) * parseFloat(custom_unit_array[cd_2_unit.value]);
 dist_array[dist_array.length - 2] = custom_distance_1;
 dist_array[dist_array.length - 1] = custom_distance_2;
 
 update_pace_table();
 
-function on_fine_slider_input() {
-    speed_kph_var = parseFloat(Number(coarse_slider.value) + Number(fine_slider.value)).toFixed(5);
+function on_fine_slider_input(event, ui) {
+    var coarse_val = $("#coarseSlider").slider( "option", "value" );
+    var fine_val = $("#fineSlider").slider( "option", "value" );
+    speed_kph_var = parseFloat(Number(coarse_val) + Number(fine_val)).toFixed(5);
     kph_cell.innerHTML = parseFloat(speed_kph_var).toFixed(3);
     mph_cell.innerHTML = parseFloat(speed_kph_var * 0.6213712).toFixed(3);
 
     update_pace_table();
 }
 
-function on_coarse_slider_input() {
+function on_coarse_slider_input(event, ui) {
     fine_slider.value = 0.0;
-    on_fine_slider_input();
+    on_fine_slider_input(event, ui);
 }
 
-coarse_slider.oninput = on_coarse_slider_input;
-coarse_slider.onclick = on_coarse_slider_input;
-fine_slider.oninput = on_fine_slider_input;
-fine_slider.onclick = on_fine_slider_input;
-
 function on_custom_dist_input() {
-    custom_distance_1 = parseFloat(cd_1_val.value) * parseFloat(cd_1_unit.value);
-    custom_distance_2 = parseFloat(cd_2_val.value) * parseFloat(cd_2_unit.value);
+    custom_distance_1 = parseFloat(cd_1_val.value) * parseFloat(custom_unit_array[cd_1_unit.value]);
+    custom_distance_2 = parseFloat(cd_2_val.value) * parseFloat(custom_unit_array[cd_2_unit.value]);
     dist_array[dist_array.length - 2] = custom_distance_1;
     dist_array[dist_array.length - 1] = custom_distance_2;
 
